13079d00a7bcea97333c723d97ed7cf51f5fb1fe
ishmodi29 <ishmodi3@gmail.com>
2020-06-04 16:18:01 +0530

    Updates

---
+++ precache-manifest.66be67bf261832b8d17485751b35ab00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64391c595df034564fcd2d4b7483e91",
+    "url": "/Expense-tracker/index.html"
+  },
+  {
+    "revision": "aa28ace8697b7ece474f",
+    "url": "/Expense-tracker/static/css/main.09fdd186.chunk.css"
+  },
+  {
+    "revision": "5ae3a01a22f252002626",
+    "url": "/Expense-tracker/static/js/2.58790b2f.chunk.js"
+  },
+  {
+    "revision": "95c4385ee90a2cc1dd72ac0d3c4df105",
+    "url": "/Expense-tracker/static/js/2.58790b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa28ace8697b7ece474f",
+    "url": "/Expense-tracker/static/js/main.cca10e98.chunk.js"
+  },
+  {
+    "revision": "bd6d5f0792eb6051b565",
+    "url": "/Expense-tracker/static/js/runtime-main.4d8b31d9.js"
+  }
+]);
